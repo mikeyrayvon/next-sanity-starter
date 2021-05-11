195bfe9180098164a23193b0bf1d60f756de401a
mikeyrayvon <mikey@interglobal.vision>
2021-05-11 16:09:31 +0200

    add post and seoMeta schemas

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -4,7 +4,12 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-import siteConfig from './documents/siteConfig';
+// Import documents
+import post from './documents/post'
+import siteConfig from './documents/siteConfig'
+
+// Import objects
+import seoMeta from './objects/seoMeta'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,6 +19,8 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    post,
+    seoMeta,
     siteConfig
   ]),
 })
