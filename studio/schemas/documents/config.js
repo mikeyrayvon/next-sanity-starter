3767dd81576e3fd7627d509840aa4e48808d2186
mikeyrayvon <mikey@interglobal.vision>
2021-07-28 23:58:40 +0200

    update sanity api values. update site title and url in config schema

--- studio/schemas/documents/config.js
+++ studio/schemas/documents/config.js
@@ -83,8 +83,8 @@ export default {
     },
   ],
   initialValue: {
-    siteTitle: 'Starter',
-    siteUrl: 'http://localhost:3000'
+    siteTitle: 'Faltpavillon',
+    siteUrl: 'https://faltpavillon.land'
   },
   preview: {
     prepare() {
