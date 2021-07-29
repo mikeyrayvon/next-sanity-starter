5bbecc317bbc37bd0de0cf0389a432f99c4772dd
mikeyrayvon <mikey@interglobal.vision>
2021-07-29 17:10:12 +0200

    update favicon

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -13,10 +13,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" href="/favicon.png" />
 
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
