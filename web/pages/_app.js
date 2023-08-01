998492c1843a7166aed939d7d0946f9fca995829
Michael Ray-Von <michaelrayvon@gmail.com>
2023-08-01 13:21:08 +0200

    upgrade to next 13

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,26 +1,26 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import 'styles/globals.css'
-import * as gtag from 'utils/gtag'
-import { StateProvider } from 'utils/store.js';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import "styles/globals.css";
+import * as gtag from "utils/gtag";
+import { StateProvider } from "utils/store.js";
 
 const App = ({ Component, pageProps }) => {
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router]);
 
   return (
     <StateProvider>
       <Component {...pageProps} />
     </StateProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,48 +1,48 @@
-import { getClient } from 'utils/sanity'
-import { configQuery } from 'utils/queries'
-import Head from 'next/head'
-import groq from 'groq'
-import Link from 'next/link'
+import { getClient } from "utils/sanity";
+import { configQuery } from "utils/queries";
+import Head from "next/head";
+import groq from "groq";
+import Link from "next/link";
 
-import Layout from 'components/Layout'
-import Seo from 'components/Seo'
-import Container from 'components/Container'
+import Layout from "components/Layout";
+import Seo from "components/Seo";
+import Container from "components/Container";
 
 const query = groq`
   *[_type == 'post']
-`
+`;
 
 const Landing = ({ docs, config }) => {
   return (
     <Layout>
       <Seo config={config} />
-      {docs && docs.length > 0 &&
+      {docs && docs.length > 0 && (
         <section>
           <Container>
-            <ul className='list-disc'>
-              {docs.map(doc => {
+            <ul className="list-disc">
+              {docs.map((doc) => {
                 return (
                   <li key={doc._id}>
-                    <Link href={`/${doc.slug.current}`} >
-                      <a className='underline'>{doc.title}</a>
+                    <Link href={`/${doc.slug.current}`} className="underline">
+                      {doc.title}
                     </Link>
                   </li>
-                )
+                );
               })}
             </ul>
           </Container>
         </section>
-      }
+      )}
     </Layout>
-  )
-}
+  );
+};
 
 export const getStaticProps = async () => {
-  const docs = await getClient(true).fetch(query)
-  const config = await getClient().fetch(configQuery)
+  const docs = await getClient(true).fetch(query);
+  const config = await getClient().fetch(configQuery);
   return {
-    props: { docs, config } // will be passed to the page component as props
-  }
-}
+    props: { docs, config }, // will be passed to the page component as props
+  };
+};
 
-export default Landing
+export default Landing;
