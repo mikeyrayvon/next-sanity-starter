424c638350050ff85dc74e6504be147ff3ada6b8
Michael Ray-Von <michaelrayvon@gmail.com>
2023-08-05 11:41:43 +0200

    move components and pages to tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -3,8 +3,9 @@ import { useRouter } from "next/router";
 import "styles/globals.css";
 import * as gtag from "utils/gtag";
 import { StateProvider } from "utils/store.js";
+import type { AppProps } from "next/app";
 
-const App = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url) => {
